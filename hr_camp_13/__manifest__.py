bf87f5b67066b7d6cc3a74f7a26d2d3f3798104f
essamcis <58879491+essamcis@users.noreply.github.com>
2020-01-20 15:43:02 +0400

    Update __manifest__.py

--- hr_camp_13/__manifest__.py
+++ hr_camp_13/__manifest__.py
@@ -5,6 +5,8 @@
     'summary': """
         Employee accommodation/Camp Managment """,
 
+ 
+
     'description': """
         add accommodation /Camp Module to system 
         allow camp boss to assign room for each employee
@@ -23,7 +25,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'hr',
+    'category': 'Human Recources',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
